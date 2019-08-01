af0311f2c48557edd5079ff928b0246c4257ba82
Instafluff <35773245+instafluff@users.noreply.github.com>
2019-08-01 13:37:46 -0700

    New Fun and Fluffy Facts!

--- index.js
+++ index.js
@@ -1,5 +1,56 @@
 require("dotenv").config();
 const fetch = require("node-fetch");
+var fs = require('fs');
+var batfacts = fs.readFileSync('batfact.txt').toString().replace(/\r/g,"").split("\n");
+var batimages = undefined;
+var wolffacts = fs.readFileSync('wolffact.txt').toString().replace(/\r/g,"").split("\n");
+var wolfimages = undefined;
+var monsfacts = fs.readFileSync('monsfact.txt').toString().replace(/\r/g,"").split("\n");
+var monsimages = undefined;
+
+function getRandomInt( number ) {
+  return Math.floor( number * Math.random() );
+}
+
+const ComfyWeb = require( "webwebweb" );
+ComfyWeb.APIs[ "/catfact" ] = async ( qs ) => {
+  return await getCatFact();
+};
+ComfyWeb.APIs[ "/pokefact" ] = async ( qs ) => {
+  return await getPokemonFact();
+};
+ComfyWeb.APIs[ "/batfact" ] = async ( qs ) => {
+  if( !batimages ) {
+    let images = await fetch( "https://unsplash.com/napi/search/photos?query=bat&per_page=50" ).then( res => res.json() );
+    batimages = images.results.map( x => x.urls.regular );
+    // console.log( batimages );
+  }
+  return {
+    fact: batfacts[ getRandomInt( batfacts.length ) ],
+    photo: batimages[ getRandomInt( batimages.length ) ]
+  };
+};
+ComfyWeb.APIs[ "/wolffact" ] = async ( qs ) => {
+  if( !wolfimages ) {
+    let images = await fetch( "https://unsplash.com/napi/search/photos?query=wolf&per_page=50" ).then( res => res.json() );
+    wolfimages = images.results.map( x => x.urls.regular );
+  }
+  return {
+    fact: wolffacts[ getRandomInt( wolffacts.length ) ],
+    photo: wolfimages[ getRandomInt( wolfimages.length ) ]
+  };
+};
+ComfyWeb.APIs[ "/monsfact" ] = async ( qs ) => {
+  if( !monsimages ) {
+    let images = await fetch( "https://unsplash.com/napi/search/photos?query=mars&per_page=50" ).then( res => res.json() );
+    monsimages = images.results.map( x => x.urls.regular );
+  }
+  return {
+    fact: monsfacts[ getRandomInt( monsfacts.length ) ],
+    photo: monsimages[ getRandomInt( monsimages.length ) ]
+  };
+};
+ComfyWeb.Run( 8999 );
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
@@ -32,6 +83,33 @@ function getCatFact() {
   });
 }
 
+function getPokemonFact() {
+  return new Promise( async (res, rej) => {
+    try {
+      var pokeId = getRandomInt( 807 ) + 1;
+      let fact = await fetch( "https://pokeapi.co/api/v2/pokemon-species/" + pokeId ).then( res => res.json() );
+      // console.log( fact );
+      let entry = fact.flavor_text_entries.find( x => x.language.name === "en" );
+      res( { name: fact.name, fact: entry.flavor_text.replace( /\n/g, " " ), photo: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + pokeId + ".png" } );
+    }
+    catch( ex ) {
+      rej( ex );
+    }
+  });
+}
+
+function getFact( type ) {
+  return new Promise( async (res, rej) => {
+    try {
+      let fact = await fetch( "http://localhost:8999/" + type ).then( res => res.json() );
+      res( fact );
+    }
+    catch( ex ) {
+      rej( ex );
+    }
+  });
+}
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -45,10 +123,40 @@ client.on('message', async msg => {
       msg.channel.send( "**Cat Fact:** " + catFact.fact, attachment );
       // msg.channel.send("**Cat Fact:** " + catFact.fact, {files: [ catFact.photo ] });
     }
+    if( msg.content === "!pokefact" ) {
+      let pokeFact = await getPokemonFact();
+      console.log( pokeFact );
+      const attachment = new Discord.Attachment( pokeFact.photo, "pokemon.jpg" );
+      msg.channel.send( "**Pokémon Fact:** " + pokeFact.name + " - " + pokeFact.fact, attachment );
+    }
+    if( msg.content === "!batfact" ) {
+      let fact = await getFact( "batfact" );
+      console.log( fact );
+      const attachment = new Discord.Attachment( fact.photo, "fact.jpg" );
+      msg.channel.send( "**Fun Fact:** " + fact.fact, attachment );
+      // msg.channel.send("**Cat Fact:** " + catFact.fact, {files: [ catFact.photo ] });
+    }
+    if( msg.content === "!wolffact" ) {
+      let fact = await getFact( "wolffact" );
+      console.log( fact );
+      const attachment = new Discord.Attachment( fact.photo, "fact.jpg" );
+      msg.channel.send( "**Fun Fact:** " + fact.fact, attachment );
+      // msg.channel.send("**Cat Fact:** " + catFact.fact, {files: [ catFact.photo ] });
+    }
+    if( msg.content === "!monsfact" ||
+        msg.content === "!marsfact" ) {
+      let fact = await getFact( "monsfact" );
+      console.log( fact );
+      const attachment = new Discord.Attachment( fact.photo, "fact.jpg" );
+      msg.channel.send( "**Fun Fact:** " + fact.fact, attachment );
+      // msg.channel.send("**Cat Fact:** " + catFact.fact, {files: [ catFact.photo ] });
+    }
   }
   catch( ex ) {
     console.log( ex );
   }
 });
+
+client.on( "error", console.error );
 
 client.login( process.env.DISCORD_TOKEN );
